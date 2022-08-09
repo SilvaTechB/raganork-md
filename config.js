01f6956c802c4ecf260b1ee76128c7f5e6610196
alien2804 <109579232+alien2804@users.noreply.github.com>
2022-08-09 01:11:48 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,16 @@ DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 module.exports = {
     VERSION: 'V 2.0.1',
-    ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}"
-    #Ubutton\Follow on Instagram# #url\https://Instagram.com/ag_toons#,
+    ALIVE: process.env.ALIVE || "https://i.imgur.com/QgIMf8b.jpeg
+ {pp} Active since {uptime}
+ User:{sender}
+
+MY QUOTE FOR YOU
+{quote}
+
+#ubutton\Instagram# #url\https://www.instagram.com/ag_toons#
+#ubutton\youtube# #url\https://www.youtube.com/channel/UCRIk9I95OZqP07CpQcEHbyw#
+#cbutton\contact owner# #url\https.//wa.me/+2348100236360",
     BLOCK_CHAT: process.env.BLOCK_CHAT || '',
     ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || false,
     READ_MESSAGES: convertToBool(process.env.READ_MESSAGES) || false,
@@ -23,13 +31,13 @@ module.exports = {
     BOT_INFO: process.env.BOT_INFO || 'Alien, Ibrahim Bashir;2348100236360;https://i.imgur.com/st4R2dq.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
     RBG_KEY: process.env.RBG_KEY || '',
     ALLOWED: process.env.ALLOWED || '91,94,212',
-    CHATBOT: process.env.CHATBOT || 'off',
+    CHATBOT: process.env.CHATBOT || 'on',
     HANDLERS: process.env.HANDLERS || '.,',
     STICKER_DATA: process.env.STICKER_DATA,
     BOT_NAME: process.env.BOT_NAME || 'Alien',
     AUDIO_DATA: process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private" ? 'ꪶ͢٭𝑺𝜣𝑼𝑹𝛢𝑽𝑲𝑳¹¹ꫂ;Raganork MD bot;https://www.linkpicture.com/q/rgnk.jpg' : process.env.AUDIO_DATA,
     TAKE_KEY: process.env.TAKE_KEY || '',
-    MODE: process.env.MODE || 'private',
+    MODE: process.env.MODE || 'public',
     WARN: process.env.WARN || '3',
     ANTILINK_WARN: process.env.ANTILINK_WARN || '',
     HEROKU: {
